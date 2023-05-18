89b42fd8c3b16a66f6ceefdef1c506fd2383ee40
Syed Faiz <faizmiya110@gmail.com>
2023-05-18 10:59:47 +0530

    first commit 3

--- resources/views/email/data_info.blade.php
+++ resources/views/email/data_info.blade.php
@@ -7,8 +7,8 @@
 <body>
     <h1>Updates</h1>
     <p>{{ @$text1 }}</p>
-    <p>{{ @$text1 }}</p>
-    <p>{{ @$text1 }}</p>
+    <p>{{ @$text2 }}</p>
+    <p>{{ @$text3 }}</p>
     <p>{{ @$msg }}</p>
 </body>
 
